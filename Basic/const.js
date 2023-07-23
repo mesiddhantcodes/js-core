27b230a75dfb55282e9fa0c38a26fcd4efadabf6
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-23 10:34:35 +0530

    const

---
+++ Basic/const.js
@@ -0,0 +1,7 @@
+// const is similar to the let but even more strict than the let
+const b=100;
+// if we assgin some value in const after the declaration then we got an error of syntax error
+// if we assign some another value to const then we got an typerroe
+// ---------
+
+// to avoid the temporal dead zone we have to declare all varible at the top 
