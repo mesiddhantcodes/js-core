449410c7b5bcc8f34d1881aa3ff1f15dd5311105
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-23 11:25:03 +0530

    fucntion

--- Basic/funct.js
+++ Basic/funct.js
@@ -7,3 +7,14 @@ var siddhant=function()
 {
     //body
 };
+
+var isEven=(ele)=>
+ {
+    // if(ele%2===0)
+    // {
+    //     return true;
+    // }
+    // return false;
+    return ele%2===0;
+    // when we use arrow function then we have to use  return 
+ };
