ae13542cc35a050bd6bb1273dd5407ce3d3f0a42
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-23 11:03:35 +0530

    Bloack and shadowing

---
+++ Basic/block.js
@@ -0,0 +1,29 @@
+// {
+//     // compund statement
+//     var a=20;
+//     console.log(a);
+
+// }
+// if(true)
+// {
+//     // block of code is written in the this block 
+// }
+
+{
+    var a=200;
+    let b=100;
+    const c=2;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+// above is the exmple of block scope in js
+
+
+// Shadowing
+// if var is also declared outside the block then the var a of block is shadow the outside var
+// var of inside block is also modified the value of a
+ 
