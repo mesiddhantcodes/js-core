b2cb471e2b5f9227171b495e16e31acbcf2c2898
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-23 11:23:44 +0530

    Bloack and shadowing

--- Basic/block.js
+++ Basic/block.js
@@ -27,10 +27,38 @@ console.log(c);
 // if var is also declared outside the block then the var a of block is shadow the outside var
 // var of inside block is also modified the value of a(var)
 
-// let and const have a blocked scope and outsode the block have a another scope
+// let and const have a blocked scope and outside the block have a another scope
 
 // three type of scope 
 // 1.global
 // 2.block 
 // 3. memory of let and const
 
+// Shadowing is also valid in function 
+var c=100;
+function s()
+{
+    var c=10;
+    console.log(c);
+}
+s();
+
+console.log(c);
+
+// illegal shadowing 
+let s=10;
+{
+    var s=0;
+    // if u want to shadow the let variale inside the block using the var it is not poosible
+    // if var is declare outide the block and want to shadow the var using let in block is poosible
+
+}
+// block scope also follow the lexical scope 
+const a=180;
+{
+    const a=100;
+    {
+        const a=1000;
+        console.log(a);
+    }
+}
