2aeff320421d41b6fc548220b03a17b172a43d7f
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-26 12:51:37 +0530

    deep dive in closure

--- Basic/setTimeout.js
+++ Basic/setTimeout.js
@@ -1,3 +1,6 @@
+// Deep dive in closure 
+
+
 function x()
 {
     var i=1;
@@ -16,6 +19,7 @@ x();
 function x() {
     for (var i = 1; i <= 5; i++) {
         // if we use let at  the place of var whcih is blocked scope 
+        // -------
         // each and every time of loop setTimeout is called and function form a closure with a new variable itself
         // the copy of i in each iteration is new 
         // here let is used beacuse of let is blocked scope and create a new copy every tiem 
@@ -23,7 +27,7 @@ function x() {
         setTimeout(function () {
             console.log(i);
             // this i is pointing the same spot in the memory of the i
-
+            // remember the same reference of i 
         }, i * 1000);
     }
     console.log("Namaste Siddhant");
@@ -66,3 +70,34 @@ x();
 
 // using this method we create a new copy of i 
 // which lead to call everytime setTimeout function which store in the separate memory space
+// ---------------------------------------------------------------------
+// closure is a combinaiton of function and lexical Scope
+// each and every function in javascrpit has access to its outer lexical environment 
+// acess to the variable and funciton which is present in the environment of its parent 
+
+// if function is executed in other scope but its still remember outer lexical environment where it is orginaly present 
+
+// 
+// 
+
+function outest()
+{
+    var c=20;
+    function outer(b)
+    {
+        function inner()
+        {
+            console.log(a,b,c);
+        }
+        let a=10;
+        return inner;
+    }
+    return outer;
+}
+var close=outest()("helowrld");
+// this will return the outer function
+
+var close =(outest())("hellow");
+// this will return the inner function
+
+close();
