4d0f79595161c216477246b96da3f59d0cd27830
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-28 10:46:00 +0530

    event listener

--- Basic/14_clickhandling.js
+++ Basic/14_clickhandling.js
@@ -6,3 +6,9 @@ function aa() {
     })
 }
 aa();
+
+// why do we remove the event listener
+// event listener are heavy (it takes memory) when we attach event it form a closure 
+// even call stack is empty but these program takes a memory and not freeing the memory
+// 
+// when we remove the event listener then all the variavble which is held by the closure will be garbage collected
