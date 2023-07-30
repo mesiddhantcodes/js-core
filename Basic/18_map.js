7df2fb7943331da7ae05d88d74c08b53cb70c286
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-07-30 18:46:13 +0530

    Map Explanation with example

--- Basic/18_map.js
+++ Basic/18_map.js
@@ -23,3 +23,15 @@ const outputt=arr.map(function bin(x)
 });
 
 const outputtt=arr.map((x)=>x.toString(2));
+
+// ================================================================================================
+const users=[
+    {firstName:"Siddhant",lastName:"kr",age:23},
+    {firstName:"tanishqa",lastName:"sharma",age:43},
+    {firstName:"mishal",lastName:"shahari",age:44},
+    {firstName:"Siddhant",lastName:"kr",age:23},
+
+
+];
+const outpuft=users.map((x)=>x.firstName+" "+x.lastName)
+
